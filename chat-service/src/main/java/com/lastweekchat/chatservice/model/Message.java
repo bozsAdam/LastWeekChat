5db625ea72817cc1fa16df7bbbfba21e84c12cd8
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-01 14:43:12 +0200

    Add configuration, postgresql

--- chat-service/src/main/java/com/lastweekchat/chatservice/model/Message.java
+++ chat-service/src/main/java/com/lastweekchat/chatservice/model/Message.java
@@ -1,12 +1,19 @@
 package com.lastweekchat.chatservice.model;
 
-import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Data
+@Entity
 public class Message {
+
+    @GeneratedValue
+    @Id
+    Long id;
 
     private String message;
     private LocalDate timeStamp;
